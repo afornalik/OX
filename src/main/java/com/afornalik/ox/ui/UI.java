53ffc00f163715829f3a1be56bcc33f7e11d36a6
Andrzej_Fornalik <Andrzej_Fornalik@epam.com>
2019-12-18 13:02:00 +0100

    Print Greeting to player

---
+++ src/main/java/com/afornalik/ox/ui/UI.java
@@ -0,0 +1,7 @@
+package com.afornalik.ox.ui;
+
+public class UI {
+    public void sayHello() {
+        System.out.println("Welcome to game OX");
+    }
+}
---
+++ src/test/java/com/afornalik/ox/ui/TestUI.java
@@ -0,0 +1,22 @@
+package com.afornalik.ox.ui;
+
+import org.mockito.BDDMockito;
+import org.mockito.Mockito;
+import org.testng.annotations.Test;
+
+@Test
+public class TestUI {
+
+
+    //not completed -> need to know how to check output method
+    public void shouldPrintGreetingsToUser() {
+        //given
+        UI ui = new UI();
+
+        //when
+        ui.sayHello();
+
+        //then
+    }
+
+}
