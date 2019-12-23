01d5fac4ac72719eb26871a4b17a44821e55de8c
Andrzej_Fornalik <Andrzej_Fornalik@epam.com>
2019-12-23 09:36:03 +0100

    Convert coordinates into map index more test

--- src/test/java/com/afornalik/ox/board/TestMoveConvert.java
+++ src/test/java/com/afornalik/ox/board/TestMoveConvert.java
@@ -8,10 +8,11 @@ import static org.assertj.core.api.Assertions.*;
 public class TestMoveConvert {
 
     private static final int BOUND_THREE = 3;
+    private static final int BOUND_SEVENTEEN = 17;
     private static final int INDEX_X_2 = 2;
     private static final int INDEX_Y_2 = 2;
-
-
+    private static final int INDEX_X_10 = 10;
+    private static final int INDEX_Y_13 = 13;
 
     public void shouldTakeTwoParameterAndTransformThemIntoAppropriateIndexNumber() {
         //given
@@ -22,5 +23,15 @@ public class TestMoveConvert {
 
         //then
         assertThat(result).isEqualTo(4);
+    }
+    public void shouldTakeTwoParameterAndTransformThemIntoAppropriateIndexNumber2() {
+        //given
+        MoveConvert moveConvert = new MoveConvert(BOUND_SEVENTEEN);
+
+        //when
+        int result = moveConvert.convertToIndex(INDEX_X_10,INDEX_Y_13);
+
+        //then
+        assertThat(result).isEqualTo(213);
     }
 }
