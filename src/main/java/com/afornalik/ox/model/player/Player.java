a995cc530b7999eb7fb5e526366edb253ff15e5d
Andrzejf <afornalik2@gmail.com>
2019-12-28 19:02:02 +0100

    Player have multiple settings

--- src/main/java/com/afornalik/ox/model/player/Player.java
+++ src/main/java/com/afornalik/ox/model/player/Player.java
@@ -1,10 +1,27 @@
 package com.afornalik.ox.model.player;
 
+import com.afornalik.ox.model.board.FieldStatus;
+
 class Player {
 
-
+    private String name = "player" ;
+    private int score = 0;
+    private FieldStatus sign = FieldStatus.EMPTY;
+    private boolean first = true;
 
     int getScore() {
-        return 0;
+        return score;
+    }
+
+    String getName() {
+        return name;
+    }
+
+    FieldStatus getSign() {
+        return sign;
+    }
+
+    boolean isFirst() {
+        return first;
     }
 }
--- src/test/java/com/afornalik/ox/model/player/TestPlayer.java
+++ src/test/java/com/afornalik/ox/model/player/TestPlayer.java
@@ -1,5 +1,6 @@
 package com.afornalik.ox.model.player;
 
+import com.afornalik.ox.model.board.FieldStatus;
 import org.testng.annotations.Test;
 
 import static org.assertj.core.api.Assertions.*;
@@ -7,12 +8,25 @@ import static org.assertj.core.api.Assertions.*;
 @Test
 public class TestPlayer {
 
-    public void shouldCreatePlayer() {
+    public void shouldCreatePlayerWithScore() {
         //when
         Player player = new Player();
 
         //then
         assertThat(player.getScore()).isEqualTo(0);
+    }
+
+    public void shouldCreatePlayerWithAllStatistics() {
+        //when
+        Player player = new Player();
+
+        //then
+        assertThat(player.getName()).isEqualTo("player");
+        assertThat(player.getScore()).isEqualTo(0);
+        assertThat(player.getSign()).isEqualTo(FieldStatus.EMPTY);
+        assertThat(player.isFirst()).isEqualTo(true);
+
 
     }
+
 }
